6fa888deebcdd0c09337c117048088d7acd953ea
salihacr <salihacur1@gmail.com>
2022-03-12 14:20:37 +0300

    allow cors from api

--- server/API/Startup.cs
+++ server/API/Startup.cs
@@ -34,6 +34,8 @@ namespace API
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddCors();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +51,10 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(opt =>
+            {
+                opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+            });
 
             app.UseAuthorization();
 
