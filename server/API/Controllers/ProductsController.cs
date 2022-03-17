bd81e4c9b120369fdd164cccbb141399a262fd9f
salihacr <salihacur1@gmail.com>
2022-03-17 17:52:34 +0300

    product filtering, searching and sorting added to api

--- server/API/Controllers/ProductsController.cs
+++ server/API/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using API.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,15 +18,25 @@ namespace API.Controllers
             _context = context;
         }
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts(
+         string orderBy,
+         string searchText,
+         string brands,
+         string types)
         {
-            return await _context.Products.ToListAsync();
+            var query = _context.Products
+            .Sort(orderBy)
+            .Search(searchText)
+            .Filter(brands, types)
+            .AsQueryable();
+
+            return await query.ToListAsync();
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
             var product = await _context.Products.FindAsync(id);
-            if(product == null) return NotFound();
+            if (product == null) return NotFound();
             return Ok(product);
         }
     }
